76533efda83fa50d921067eb3f2585cc48ab479b
Cory <coryf65@gmail.com>
2019-05-15 09:09:12 -0500

    Add project files.

---
+++ ClassLibraryCoryTest/SampleLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLibraryCoryTest
+{
+    public class SampleLibrary
+    {
+        public double Addition(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtraction(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiplication(double x, double y)
+        {
+            return x * y;
+        }
+        public double Division(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
